d5a339bf52da1eb8c63e586a21982c47bbe32707
welligton <welligton.costa@pollvo.com>
2025-05-20 14:25:34 -0300

    fix: correcao do download

--- projeto_selecao_aleatoria/projeto_selecao_aleatoria/urls.py
+++ projeto_selecao_aleatoria/projeto_selecao_aleatoria/urls.py
@@ -38,7 +38,9 @@ router.register(r'rpa', RPAViewSet, basename='rpa')
 router.register(r'historico-rpa', HistoricoRPAViewSet, basename='histforico-rpa')
 router.register(r'docker-historico', DockerHistoricoViewSet, basename='docker-historico')
 router.register(r'docker-rpa', RPADockerViewSet, basename='docker-rpa')
-router.register(r'resultados', ResultadoDownloadViewSet, basename='resultados')
+#router.register(r'resultados', ResultadoDownloadViewSet, basename='resultados') 
+router.register(r'rpa/resultados', ResultadoDownloadViewSet, basename='resultados')
+
 
 # Router para as APIs de processamento por usuário
 router.register(
@@ -51,8 +53,8 @@ router.register(
     UserDockerProcessamentoViewSet,
     basename='user-docker-processamento'
 )
-router.register(r'rpa/resultados', ResultadoDownloadViewSet, basename='resultados') 
-router.register(r'rpa', RPAViewSet, basename='rpa')
+ 
+
 
 
 
